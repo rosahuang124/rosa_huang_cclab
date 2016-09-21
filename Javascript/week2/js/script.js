ad8e3c626356af6592cd352c1ea43847ef7bdc89
rosahuang124 <rosa@RosadeMBP.fios-router.home>
2016-09-20 23:55:21 -0400

    finish week2 HW

--- Javascript/week2/js/script.js
+++ Javascript/week2/js/script.js
@@ -2,43 +2,38 @@ var userChoice;
 var computerChoice;
 
 function playStone(){
-	if(computerChoice = "stone"){
 		$('#computer').css("background", "url('img/stone-r.png') no-repeat");
-	} 
 }
 
 function playPaper(){
-	if(computerChoice = "paper"){
 		$('#computer').css("background", "url('img/paper-r.png') no-repeat");
-	} 
 }
 
 function playScissors(){
-	if(computerChoice = "paper"){
 		$('#computer').css("background", "url('img/scissors-r.png') no-repeat");
-	} 
 }
 
 function userPaper(){
-	$('#choose-paper').click($('#player').css("background", "url('img/paper-l.png') no-repeat"));
-	var userChoice = "paper";
+	$('#player').css("background", "url('img/paper-l.png') no-repeat");
+	userChoice = "paper";
 	console.log(userChoice);
 }
 
 function userScissors(){
-	$('#choose-scissors').click($('#player').css("background", "url('img/scissors-l.png') no-repeat"));
-	var userChoice = "scissors";
+	$('#player').css("background", "url('img/scissors-l.png') no-repeat");
+	userChoice = "scissors";
+
 	console.log(userChoice);
 }
 
 function userStone(){
-	$('#choose-stone').click($('#player').css("background", "url('img/stone-l.png') no-repeat"));
-	var userChoice = "stone";
+	$('#player').css("background", "url('img/stone-l.png') no-repeat");
+	userChoice = "stone";
 	console.log(userChoice);
 }
 
 function startPlay(){
-	var computerChoice = Math.random();
+	computerChoice = Math.random();
 
 	if(computerChoice < 0.34){
 		computerChoice = "stone";
@@ -51,6 +46,7 @@ function startPlay(){
 		playScissors();
 	}
 	console.log("computer choose " + computerChoice)
+	compare();
 }
 
 
@@ -58,63 +54,64 @@ function startPlay(){
 var compare = function(){
 	if(userChoice === "paper"){
 		if(computerChoice === "stone") {
-			console.log("win");
-			// swal({   
-			// title: "You win!",   
-			// text: "Congratulation",   
-			// imageUrl: "img/win.png" });
+			console.log("win");	
+				swal({   
+				title: "You win!",   
+				text: "Congratulation",   
+				imageUrl: "img/win.png" });
+			
 		} else if(computerChoice === "scissors"){
 			console.log("loose");
-			// swal({   
-			// title: "You Loose...",   
-			// text: "Try again?",   
-			// imageUrl: "img/loose.png" });			
+			swal({   
+			title: "You Loose...",   
+			text: "Try again?",   
+			imageUrl: "img/loose.png" });			
 		} else {
 			console.log("draw");
-			// swal({   
-			// title: "It's A Draw!",   
-			// text: "Try again?",   
-			// imageUrl: "img/drawn.png" })			
+			swal({   
+			title: "It's A Draw!",   
+			text: "Try again?",   
+			imageUrl: "img/drawn.png" })			
 		}
 	} else if(userChoice === "scissors"){
 		if(computerChoice === "paper") {
 			console.log("win");
-			// swal({   
-			// title: "You win!",   
-			// text: "Congratulation",   
-			// imageUrl: "img/win.png" });
+			swal({   
+			title: "You win!",   
+			text: "Congratulation",   
+			imageUrl: "img/win.png" });
 		} else if(computerChoice === "stone"){
 			console.log("loose");
-			// swal({   
-			// title: "You Loose...",   
-			// text: "Try again?",   
-			// imageUrl: "img/loose.png" });			
+			swal({   
+			title: "You Loose...",   
+			text: "Try again?",   
+			imageUrl: "img/loose.png" });			
 		} else {
 			console.log("draw");
-			// swal({   
-			// title: "It's A Draw!",   
-			// text: "Try again?",   
-			// imageUrl: "img/drawn.png" })			
+			swal({   
+			title: "It's A Draw!",   
+			text: "Try again?",   
+			imageUrl: "img/drawn.png" })			
 		}
 	} else if(userChoice === "stone"){
 		if(computerChoice === "scissors") {
 			console.log("win");
-			// swal({   
-			// title: "You win!",   
-			// text: "Congratulation",   
-			// imageUrl: "img/win.png" });
+			swal({   
+			title: "You win!",   
+			text: "Congratulation",   
+			imageUrl: "img/win.png" });
 		} else if(computerChoice === "paper"){
 			console.log("loose");
-			// swal({   
-			// title: "You Loose...",   
-			// text: "Try again?",   
-			// imageUrl: "img/loose.png" });			
+			swal({   
+			title: "You Loose...",   
+			text: "Try again?",   
+			imageUrl: "img/loose.png" });			
 		} else {
 			console.log("draw");
-			// swal({   
-			// title: "It's A Draw!",   
-			// text: "Try again?",   
-			// imageUrl: "img/drawn.png" })			
+			swal({   
+			title: "It's A Draw!",   
+			text: "Try again?",   
+			imageUrl: "img/drawn.png" })			
 		}
 	}
 }
